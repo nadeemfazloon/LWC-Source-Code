a296000d6369c50534f26233884fbbfbc56662ee
Nadeem Fazloon <mnfazloon@gmail.com>
2020-02-04 23:32:51 +0530

    Add files via upload

---
+++ Building The First LWC/helloWorld.js
@@ -0,0 +1,9 @@
+//We are importing the lightning Element base class for the lightning component from the core 'lwc' module.
+import { LightningElement } from 'lwc';
+
+//This class will contain all the JavaScript related code.
+//export default class helloWorld extends from the 'LightningElement' 
+export default class helloWorld extends LightningElement {
+
+
+}
